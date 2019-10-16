7d4aefb2c3a090d8d6bf4687303c2083a936621b
Lars Willighagen <lars.willighagen@gmail.com>
2019-10-16 14:59:02 +0200

    feat(bibtex): add type mappings

---
+++ bibtex/scripts/types.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const path = require('path')
+const { bibtex, csl } = require('../source/types')
+
+const sheets = fs
+  .readFileSync(path.join(__dirname, '../sheets/Entry Types.tsv'), 'utf8')
+  .split('\n')
+  .reduce((map, row) => {
+    const [type, required, optional, description] = row.split('\t')
+    map[type] = { required, optional, description }
+    return map
+  }, {})
+
+const bibtexTypes = Object.keys(sheets)
+const output = {
+  mappings: {
+    source: {},
+    target: {}
+  },
+  requiredFields: {}
+}
+
+for (let bibtexType of bibtexTypes) {
+  const cslType = bibtex[bibtexType]
+  if (!cslType) { continue }
+  output.mappings.source[bibtexType] = cslType
+
+  const required = sheets[bibtexType].required
+  if (!required) { continue }
+  const cleanRequired = required
+    .trim()
+    .replace(/  +/g, ' ')
+    .replace(/ (and\/)?or /g, '/')
+  output.requiredFields[bibtexType] = cleanRequired
+}
+
+for (let cslType in csl) {
+  output.mappings.target[cslType] = csl[cslType]
+}
+
+fs.writeFileSync(path.join(__dirname, '../output/types.json'), JSON.stringify(output.mappings, null, 2))
+fs.writeFileSync(path.join(__dirname, '../output/required.json'), JSON.stringify(output.requiredFields, null, 2))
